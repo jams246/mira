685428e64fac32dbd4d39a002cda925f628d349c
Jams246 <jason246@twofoursix.net>
2021-12-06 23:21:00 +0000

    Fix test to check for docker create timestamp

--- tests/Ampere/SystemInfo/ValueObject/DockerProcessValueObjectTest.php
+++ tests/Ampere/SystemInfo/ValueObject/DockerProcessValueObjectTest.php
@@ -12,20 +12,21 @@ class DockerProcessValueObjectTest extends TestCase
     {
         $this->expectException(ValueObjectException::class);
 
-        (new DockerProcessValueObject('Test1', 'Test2', -1.0, 246));
+        (new DockerProcessValueObject('Test1', 'Test2', -1.0, 246, 1638759482));
     }
 
     /**
      * @dataProvider validDockerProcessValueProvider
      */
-    public function testGetValues(string $name, string $state, float $cpu, int $memory, array $expected): void
+    public function testGetValues(string $name, string $state, float $cpu, int $memory, int $createdAt, array $expected): void
     {
-        $object = new DockerProcessValueObject($name, $state, $cpu, $memory);
+        $object = new DockerProcessValueObject($name, $state, $cpu, $memory, $createdAt);
 
         $this->assertSame($expected[0], $object->getName());
         $this->assertSame($expected[1], $object->getState());
         $this->assertSame($expected[2], $object->getCpu());
         $this->assertSame($expected[3], $object->getMemory());
+        $this->assertSame($expected[4], $object->getCreatedAt());
     }
 
     public function validDockerProcessValueProvider(): array
@@ -36,7 +37,8 @@ class DockerProcessValueObjectTest extends TestCase
                 'state',
                 1.0,
                 246,
-                ['name', 'state', 1.0, 246],
+                1638759482,
+                ['name', 'state', 1.0, 246, 1638759482],
             ],
         ];
     }
